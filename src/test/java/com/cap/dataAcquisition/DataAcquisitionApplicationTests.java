e49526b58942c2fbb40f7e7e6166b577682bb85a
ngtianxun08 <e1327862@u.nus.edu>
2025-03-06 13:13:26 +0800

    CAP-13 API Design for read service - Data Acquisition Service * Added testcontainer for postgresql test.

--- src/test/java/com/cap/dataAcquisition/DataAcquisitionApplicationTests.java
+++ src/test/java/com/cap/dataAcquisition/DataAcquisitionApplicationTests.java
@@ -1,10 +1,25 @@
 package com.cap.dataAcquisition;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class DataAcquisitionApplicationTests {
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @BeforeAll
+    static void setup() {
+        postgres.start();
+        System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgres.getUsername());
+        System.setProperty("spring.datasource.password", postgres.getPassword());
+    }
 
 	@Test
 	void contextLoads() {
